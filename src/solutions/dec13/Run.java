c7abcdaee2abee4d8810c1ffa3a67efe1aed9ffb
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-12-13 17:25:17 +0100

    Day 13 part 1 solved

---
+++ src/solutions/dec13/Run.java
@@ -0,0 +1,17 @@
+package solutions.dec13;
+
+import utils.Utils;
+
+import java.util.List;
+
+public class Run {
+    public static void main(String[] args) {
+        List<String> testInput = Utils.readFileToList("src/solutions/dec13/testInput");
+        List<String> input = Utils.readFileToList("src/solutions/dec13/input");
+        Solve13 s = new Solve13();
+
+        System.out.println(s.SolveA(testInput));
+        System.out.println(s.SolveA(input));
+        System.out.println(s.SolveB(testInput));
+    }
+}
---
+++ src/solutions/dec13/Solve13.java
@@ -0,0 +1,32 @@
+package solutions.dec13;
+
+import java.util.List;
+
+public class Solve13 {
+    public int SolveA(List<String> input){
+        int result = 0;
+        for(int i = 0; i < input.size(); i+=4){
+            int aY = Integer.parseInt(input.get(i).split(",")[1].split("\\+")[1]);
+            int aX = Integer.parseInt(input.get(i).split(",")[0].split("X\\+")[1]);
+            int bY = Integer.parseInt(input.get(i+1).split(",")[1].split("\\+")[1]);
+            int bX = Integer.parseInt(input.get(i+1).split(",")[0].split("X\\+")[1]);
+            int pY = Integer.parseInt(input.get(i+2).split("Y=")[1]);
+            int pX = Integer.parseInt(input.get(i+2).split(",")[0].split("X=")[1]);
+
+            int lowestPresses = Integer.MAX_VALUE;
+            for(int j = 1; j <= 100; j++) {
+                pY -= bY;
+                pX -= bX;
+                if (pY % aY == 0 && pX % aX == 0 && pY/aY == pX/aX) {
+                    lowestPresses = Math.min(lowestPresses, j + (pY / aY * 3));
+                }
+            }
+
+            if(lowestPresses != Integer.MAX_VALUE){
+                result += lowestPresses;
+            }
+        }
+        return result;
+    }
+
+}
