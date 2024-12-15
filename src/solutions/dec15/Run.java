1c0a6cae676b63fdbf46a0280b7fd629826a64c8
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-12-15 21:51:46 +0100

    day 15 part 2 solved

--- src/solutions/dec15/Run.java
+++ src/solutions/dec15/Run.java
@@ -6,13 +6,13 @@ import java.util.List;
 
 public class Run {
     public static void main(String[] args) {
-        List<String> smallTestInput = Utils.readFileToList("src/solutions/dec15/smallTestInput");
         List<String> testInput = Utils.readFileToList("src/solutions/dec15/testInput");
         List<String> input = Utils.readFileToList("src/solutions/dec15/input");
         Solve15 s = new Solve15();
 
-        System.out.println("Small Test A: " + s.solveA(smallTestInput));
         System.out.println("Test A: " + s.solveA(testInput));
         System.out.println("Actual A: " + s.solveA(input));
+        System.out.println("Small Test B: " + s.solveB(testInput));
+        System.out.println("Actual B: " + s.solveB(input));
     }
 }
--- src/solutions/dec15/Solve15.java
+++ src/solutions/dec15/Solve15.java
@@ -27,8 +27,8 @@ public class Solve15 {
                 int x = Integer.parseInt(position.split(":")[1]);
                 char instruction = input.get(i).charAt(j);
                 boolean emptyFound = false;
+                int counter = 1;
                 if(instruction == '^'){
-                    int counter = 1;
                     for(int k = y-1; k > 0; k--){
                         if(warehouse.get(k).get(x) == '.'){
                             emptyFound = true;
@@ -45,7 +45,6 @@ public class Solve15 {
                         position = y-1 + ":" + x;
                     }
                 }else if(instruction == 'v'){
-                    int counter = 1;
                     for(int k = y+1; k < warehouse.size(); k++){
                         if(warehouse.get(k).get(x) == '.'){
                             emptyFound = true;
@@ -62,7 +61,6 @@ public class Solve15 {
                         position = y+1 + ":" + x;
                     }
                 }else if(instruction == '<'){
-                    int counter = 1;
                     for(int k = x-1; k > 0; k--){
                         if(warehouse.get(y).get(k) == '.'){
                             emptyFound = true;
@@ -79,7 +77,6 @@ public class Solve15 {
                         position = y + ":" + (x-1);
                     }
                 }else if(instruction == '>'){
-                    int counter = 1;
                     for(int k = x+1; k < warehouse.get(0).size(); k++){
                         if(warehouse.get(y).get(k) == '.'){
                             emptyFound = true;
@@ -102,6 +99,232 @@ public class Solve15 {
             for(int j = 0; j < warehouse.get(0).size(); j++){
                 if(warehouse.get(i).get(j) == 'O'){
                     result += (i*100) + j;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public int solveB(List<String> input) {
+        int result = 0;
+        int reader = 0;
+        String position = "";
+        List<List<Character>> warehouse = new ArrayList<>();
+        while (!input.get(reader).isEmpty()) {
+            List<Character> row = new ArrayList<>();
+            for (int i = 0; i < input.get(reader).length(); i++) {
+                if (position.isEmpty() && input.get(reader).charAt(i) == '@') {
+                    position = reader + ":" + (i * 2);
+                    row.add('@');
+                    row.add('.');
+                }else if(input.get(reader).charAt(i) == '#'){
+                    row.add('#');
+                    row.add('#');
+                }else if(input.get(reader).charAt(i) == 'O'){
+                    row.add('[');
+                    row.add(']');
+                }else{
+                    row.add('.');
+                    row.add('.');
+                }
+            }
+            warehouse.add(row);
+            reader++;
+        }
+
+        for (int i = reader; i < input.size(); i++) {
+            for(char c: input.get(i).toCharArray()){
+                List<String> boxes = new ArrayList<>();
+                List<String> boxesToCheck = new ArrayList<>();
+                String[] currentPosition = position.split(":");
+                int currentY = Integer.parseInt(currentPosition[0]);
+                int currentX = Integer.parseInt(currentPosition[1]);
+                boolean allMovable = true;
+                if(c == '^'){
+                    if(warehouse.get(currentY - 1).get(currentX) == '.'){
+                        warehouse.get(currentY - 1).set(currentX, '@');
+                        warehouse.get(currentY).set(currentX, '.');
+                        position = (currentY - 1) + ":" + currentX;
+                    }else if(warehouse.get(currentY - 1).get(currentX) != '#'){
+                        if(warehouse.get(currentY - 1).get(currentX) == '['){
+                            boxesToCheck.add((currentY - 1) + ":" + currentX + "-" + (currentY - 1) + ":" + (currentX+1));
+                        }else{
+                            boxesToCheck.add((currentY - 1) + ":" + (currentX-1) + "-" + (currentY - 1) + ":" + (currentX+1));
+                        }
+                        while(!boxesToCheck.isEmpty()){
+                            allMovable = true;
+                            List<String> newBoxesToCheck = new ArrayList<>();
+                            for(String box: boxesToCheck){
+                                String[] boxBounds = box.split("-");
+                                String[] leftSide = boxBounds[0].split(":");
+                                int leftY = Integer.parseInt(leftSide[0]);
+                                int leftX = Integer.parseInt(leftSide[1]);
+                                boxes.add(box);
+                                if(warehouse.get(leftY - 1).get(leftX) == '#' || warehouse.get(leftY - 1).get(leftX+1) == '#'){
+                                    allMovable = false;
+                                    newBoxesToCheck = new ArrayList<>();
+                                    break;
+                                }
+                                if(warehouse.get(leftY - 1).get(leftX - 1) == '['){
+                                    boxes.add((leftY - 1) + ":" + (leftX - 1) + "-" + (leftY - 1) + ":" + leftX);
+                                    newBoxesToCheck.add((leftY - 1) + ":" + (leftX - 1) + "-" + (leftY - 1) + ":" + leftX);
+                                    allMovable = false;
+                                }
+                                if(warehouse.get(leftY - 1).get(leftX) == '['){
+                                    boxes.add((leftY - 1) + ":" + (leftX) + "-" + (leftY - 1) + ":" + (leftX + 1));
+                                    newBoxesToCheck.add((leftY - 1) + ":" + (leftX) + "-" + (leftY - 1) + ":" + (leftX + 1));
+                                    allMovable = false;
+                                }
+                                if(warehouse.get(leftY - 1).get(leftX + 1) == '['){
+                                    boxes.add((leftY - 1) + ":" + (leftX + 1) + "-" + (leftY - 1) + ":" + (leftX + 2));
+                                    newBoxesToCheck.add((leftY - 1) + ":" + (leftX + 1) + "-" + (leftY - 1) + ":" + (leftX + 2));
+                                    allMovable = false;
+                                }
+                            }
+                            boxesToCheck = newBoxesToCheck;
+                        }
+                    }else if(warehouse.get(currentY - 1).get(currentX) == '#'){
+                        allMovable = false;
+                    }
+                    if(allMovable){
+                        for(String box: boxes.reversed()){
+                            String[] boxBounds = box.split("-");
+                            String[] leftSide = boxBounds[0].split(":");
+                            int leftY = Integer.parseInt(leftSide[0]);
+                            int leftX = Integer.parseInt(leftSide[1]);
+                            warehouse.get(leftY - 1).set(leftX, '[');
+                            warehouse.get(leftY - 1).set(leftX + 1, ']');
+                            warehouse.get(leftY).set(leftX, '.');
+                            warehouse.get(leftY).set(leftX + 1, '.');
+                        }
+                        warehouse.get(currentY).set(currentX, '.');
+                        position = (currentY - 1) + ":" + currentX;
+                        warehouse.get(currentY - 1).set(currentX, '@');
+                    }
+                }else if(c == 'v'){
+                    if(warehouse.get(currentY + 1).get(currentX) == '.'){
+                        warehouse.get(currentY + 1).set(currentX, '@');
+                        warehouse.get(currentY).set(currentX, '.');
+                        position = (currentY + 1) + ":" + currentX;
+                    }else if(warehouse.get(currentY + 1).get(currentX) != '#'){
+                        if(warehouse.get(currentY + 1).get(currentX) == '['){
+                            boxesToCheck.add((currentY + 1) + ":" + currentX + "-" + (currentY + 1) + ":" + (currentX+1));
+                        }else{
+                            boxesToCheck.add((currentY + 1) + ":" + (currentX-1) + "-" + (currentY + 1) + ":" + (currentX+1));
+                        }
+                        while(!boxesToCheck.isEmpty()){
+                            allMovable = true;
+                            List<String> newBoxesToCheck = new ArrayList<>();
+                            for(String box: boxesToCheck){
+                                String[] boxBounds = box.split("-");
+                                String[] leftSide = boxBounds[0].split(":");
+                                int leftY = Integer.parseInt(leftSide[0]);
+                                int leftX = Integer.parseInt(leftSide[1]);
+                                boxes.add(box);
+                                if(warehouse.get(leftY + 1).get(leftX) == '#' || warehouse.get(leftY + 1).get(leftX+1) == '#'){
+                                    allMovable = false;
+                                    newBoxesToCheck = new ArrayList<>();
+                                    break;
+                                }
+                                if(warehouse.get(leftY + 1).get(leftX - 1) == '['){
+                                    boxes.add((leftY + 1) + ":" + (leftX - 1) + "-" + (leftY + 1) + ":" + leftX);
+                                    newBoxesToCheck.add((leftY + 1) + ":" + (leftX - 1) + "-" + (leftY + 1) + ":" + leftX);
+                                    allMovable = false;
+                                }
+                                if(warehouse.get(leftY + 1).get(leftX) == '['){
+                                    boxes.add((leftY + 1) + ":" + (leftX) + "-" + (leftY + 1) + ":" + (leftX + 1));
+                                    newBoxesToCheck.add((leftY + 1) + ":" + (leftX) + "-" + (leftY + 1) + ":" + (leftX + 1));
+                                    allMovable = false;
+                                }
+                                if(warehouse.get(leftY + 1).get(leftX + 1) == '['){
+                                    boxes.add((leftY + 1) + ":" + (leftX + 1) + "-" + (leftY + 1) + ":" + (leftX + 2));
+                                    newBoxesToCheck.add((leftY + 1) + ":" + (leftX + 1) + "-" + (leftY + 1) + ":" + (leftX + 2));
+                                    allMovable = false;
+                                }
+                            }
+                            boxesToCheck = newBoxesToCheck;
+                        }
+                    }else if(warehouse.get(currentY + 1).get(currentX) == '#'){
+                        allMovable = false;
+                    }
+                    if(allMovable){
+                        for(String box: boxes.reversed()){
+                            String[] boxBounds = box.split("-");
+                            String[] leftSide = boxBounds[0].split(":");
+                            int leftY = Integer.parseInt(leftSide[0]);
+                            int leftX = Integer.parseInt(leftSide[1]);
+                            warehouse.get(leftY + 1).set(leftX, '[');
+                            warehouse.get(leftY + 1).set(leftX + 1, ']');
+                            warehouse.get(leftY).set(leftX, '.');
+                            warehouse.get(leftY).set(leftX + 1, '.');
+                        }
+                        warehouse.get(currentY).set(currentX, '.');
+                        position = (currentY + 1) + ":" + currentX;
+                        warehouse.get(currentY + 1).set(currentX, '@');
+                    }
+                }else if(c == '<'){
+                    int counter = 2;
+                    if(warehouse.get(currentY).get(currentX-1) == '.'){
+                        warehouse.get(currentY).set(currentX - 1, '@');
+                        warehouse.get(currentY).set(currentX, '.');
+                        position = (currentY) + ":" + (currentX-1);
+                        allMovable = false;
+                    }else if(warehouse.get(currentY).get(currentX-1) == ']'){
+                        while(warehouse.get(currentY).get(currentX-counter-1) != '.'){
+                            counter+=2;
+                            if(warehouse.get(currentY).get(currentX-counter) == '#'){
+                                allMovable = false;
+                                break;
+                            }
+                        }
+                    }else if(warehouse.get(currentY).get(currentX - 1) == '#'){
+                        allMovable = false;
+                    }
+                    if(allMovable){
+                        for(int k = counter; k > 0; k-=2){
+                            warehouse.get(currentY).set(currentX - k, ']');
+                            warehouse.get(currentY).set(currentX - k - 1, '[');
+                        }
+                        warehouse.get(currentY).set(currentX - 1, '@');
+                        warehouse.get(currentY).set(currentX, '.');
+                        position = (currentY) + ":" + (currentX-1);
+                    }
+                }else if(c == '>'){
+                    int counter = 2;
+                    if(warehouse.get(currentY).get(currentX+1) == '.'){
+                        warehouse.get(currentY).set(currentX+1, '@');
+                        warehouse.get(currentY).set(currentX, '.');
+                        position = (currentY) + ":" + (currentX+1);
+                        allMovable = false;
+                    }else if(warehouse.get(currentY).get(currentX+1) == '['){
+                        while(warehouse.get(currentY).get(currentX+counter+1) != '.'){
+                            counter+=2;
+                            if(warehouse.get(currentY).get(currentX+counter) == '#'){
+                                allMovable = false;
+                                break;
+                            }
+                        }
+                    }else if(warehouse.get(currentY).get(currentX+1) == '#'){
+                        allMovable = false;
+                    }
+                    if(allMovable){
+                        for(int k = 0; k <= counter; k+=2){
+                            warehouse.get(currentY).set(currentX + k, '[');
+                            warehouse.get(currentY).set(currentX + k + 1, ']');
+                        }
+                        warehouse.get(currentY).set(currentX + 1, '@');
+                        warehouse.get(currentY).set(currentX, '.');
+                        position = (currentY) + ":" + (currentX+1);
+                    }
+                }
+
+            }
+        }
+        for(int i = 0; i < warehouse.size(); i++){
+            for(int j = 0; j < warehouse.get(0).size(); j++){
+                if(warehouse.get(i).get(j) == '['){
+                    result += 100 * i + j;
                 }
             }
         }
