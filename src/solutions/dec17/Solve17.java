814f1d94cac3904a686645403c5e6e39354db71b
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-12-19 19:48:05 +0100

    removed unused import

--- src/solutions/dec17/Solve17.java
+++ src/solutions/dec17/Solve17.java
@@ -3,7 +3,6 @@ package solutions.dec17;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Solve17 {
     private long a;
